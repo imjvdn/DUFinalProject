57cd06135646d4cabb1d48564c0d50ac382a0b64
Jadan Morrow <jadanmorrow@outlookcom>
2020-06-24 15:09:35 -0600

    updating all files

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import IconLink from '../buttons/IconLink';
+import Github from '../../res/footer-github.png';
+import YouTube from '../../res/footer-youtube.png';
+
+const Footer = () => {
+  return (
+    <div className="footer ">
+      <span>{/* Developed by  */}</span>
+      <IconLink
+        href={'https://github.com/imjvdn/DUFinalProject'}
+        icon={Github}
+        title="Github"
+        className="social-icon"
+        buttonStyle={{ verticalAlign: 'sub' }}
+      />
+    </div>
+  );
+};
+
+export default Footer;
