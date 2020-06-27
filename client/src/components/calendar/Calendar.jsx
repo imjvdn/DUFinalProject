0b0b1e8a368e74900f3c802f9f9f23e929c81db3
Clayton Hill <clayman0123@yahoo.com>
2020-06-27 14:17:34 -0600

    changed calendar style

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Calendar = () => {
   return (
-    <iframe src="https://calendar.google.com/calendar/embed?src=2ds5fbmt39id59im05nqdodci8%40group.calendar.google.com&ctz=America%2FDenver" title="Nitinerary" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?height=500&amp;wkst=1&amp;bgcolor=%237986CB&amp;ctz=America%2FDenver&amp;src=MmRzNWZibXQzOWlkNTlpbTA1bnFkb2RjaThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23009688&amp;showTz=0&amp;showCalendars=0&amp;showTabs=0&amp;showPrint=0&amp;showNav=1&amp;showDate=1&amp;showTitle=0" title="Nitinerary" style={{border:"solid 1px #777"}} width="500" height="500" frameborder="0" scrolling="no"></iframe>
   )
 }
 
