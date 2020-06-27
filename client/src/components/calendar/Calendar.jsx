2621d874511533c76b80330fcef91f1397b4b07d
Clayton Hill <clayman0123@yahoo.com>
2020-06-27 13:51:14 -0600

    embedded google calendar

---
+++ client/src/components/calendar/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <iframe src="https://calendar.google.com/calendar/embed?src=2ds5fbmt39id59im05nqdodci8%40group.calendar.google.com&ctz=America%2FDenver" title="Nitinerary" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  )
+}
+
+export default Calendar;
