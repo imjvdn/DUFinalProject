7ca33a1b0f34a4c331b834682f42a65c022180b2
Skip Khamvongsa <skipk95@gmail.com>
2020-06-27 21:54:01 -0600

    Added DB seed for test, and GET & POST for DB

--- client/src/components/cards/CardList.jsx
+++ client/src/components/cards/CardList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Card from "./CardSave";
-import { data } from "../../data";
+// import Card from "./Card";
+// import { data } from "../../data";
 
-const CardList = () => {
-    return data.map(app => {
-        return (
-            <Card />
-        );
-    });
+const CardList = ({ children }) => {
+    // return data.map(app => {
+    //     return (
+    //         <Card {...app} key={app.name} />
+    //     );
+    // });
+    return <li className="list-group-item">{children}</li>
 };
 
 export default CardList;
