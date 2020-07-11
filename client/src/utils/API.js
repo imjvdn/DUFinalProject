b393a023f802ca9abdf0ee781093c2de99cb0bd4
Tammy Richardson <atrich0220@gmail.com>
2020-07-11 11:37:43 -0600

    fixed restaurant URL to search for cuisines types in the denver area

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,37 +5,37 @@ const APIKEYTO = process.env.REACT_APP_TOKEY;
 
 
 export default {
-    
-    search: function(query) {
+
+    search: function (query) {
         // return axios.get(BASEURL + query + "?apikey=" + APIKEY);
         return axios.get("https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + query + "&apikey=" + APIKEY)
     },
-    getPlans: function() {
+    getPlans: function () {
         return axios.get("/api/plans");
     },
-    deletePlans: function(id) {
+    deletePlans: function (id) {
         return axios.delete("/api/plans/" + id);
     },
-    savePlans: function(newPlan) {
+    savePlans: function (newPlan) {
         console.log("Saving plans");
         console.log(newPlan);
         return axios.post("/api/plans", newPlan);
     },
-    getApi: function() {
+    getApi: function () {
         return axios.get("/api/apimodel");
     },
-    deleteApi: function(id) {
+    deleteApi: function (id) {
         return axios.delete("/api/apimodel/" + id);
     },
-    saveApi: function(savePlan) {
+    saveApi: function (savePlan) {
         return axios.post("/api/apimodel", savePlan);
     },
-   
-    searchRestaurant: function(query) {
+
+    searchRestaurant: function (query) {
         // return axios.get(BASEURL + query + "?apikey=" + APIKEY);
         console.log(query);
-        return axios.get("https://developers.zomato.com/api/v2.1/search?name=" + query + "&apikey=" + APIKEYTO)
+        return axios.get("https://developers.zomato.com/api/v2.1/search?entity_id=305&entity_type=city&cuisines=" + query + "&apikey=" + APIKEYTO)
     }
+    
 
- 
 }
